772bbe9761e25796854646e7f12838d94a0753ab
ppazos <pablo.pazos@cabolabs.com>
2023-07-28 11:15:07 -0300

    improvements for html report

--- src/PhTestRun.php
+++ src/PhTestRun.php
@@ -335,12 +335,12 @@ class PhTestRun
                         <div  class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                            <h6 class="m-0 font-weight-bold text-primary">' . $names[2] . '</h6>
                         </div>
-                           <!-- Card Body -->
+                        <!-- Card Body -->
                         <div class="card-body">
-                           <table class="table table-borderless" style="margin: -0.5rem;">
+                           <table class="table table-borderless table-responsive" style="margin: -0.5rem;">
                               <thead>
                                  <tr class ="border-bottom">
-                                    <th scope="col">Class</th>
+                                    <th scope="col">Test</th>
                                     <th scope="col">Asserts</th>
                                     <th scope="col"></th>
                                  </tr>
@@ -355,29 +355,29 @@ class PhTestRun
                {
                   foreach ($report['asserts'] as $assert_report)
                   {
-                     $html_report .= '<td>' . $test_function . '</td>';
+                     $html_report .= '<td>'. $test_function . '</td>';
                      if ($assert_report['type'] == 'ERROR')
                      {
-                        $html_report .= '<td class ="text-danger">ERROR: ' . $assert_report['msg'] . '</td>';
+                        $html_report .= '<td class ="text-danger">ERROR: '. $assert_report['msg'] .'</td>';
 
                         $total_failed++;
                         $failed++;
                      }
                      else if ($assert_report['type'] == 'OK')
                      {
-                        $html_report .= '<td class="text-success">OK: ' . $assert_report['msg'] . '</td>';
+                        $html_report .= '<td class="text-success">OK: '. $assert_report['msg'] .'</td>';
 
                         $total_successful++;
                         $successful++;
                      }
                      else if ($assert_report['type'] == 'EXCEPTION')
                      {
-                        $html_report .= '<td class="text-primary">EXCEPTION: ' . $assert_report['msg'] . '</td>';
+                        $html_report .= '<td class="text-primary">EXCEPTION: <pre>'. $assert_report['msg'] .'</pre></td>';
                      }
 
                      if (!empty($report['output']))
                      {
-                        $html_report .= '<td class="text-secondary">OUTPUT: <pre>' . $report['output'] . '</pre></td>';
+                        $html_report .= '<td class="text-secondary">OUTPUT: <pre>'. $report['output'] .'</pre></td>';
                      }
                      $html_report .= '</tr>';
                   }
@@ -386,7 +386,7 @@ class PhTestRun
                }
                else
                {
-                  $html_report .= '<td>' . $test_function . '</td>';
+                  $html_report .= '<td>'. $test_function .'</td>';
                   $html_report .= '<td></td>';
                   $html_report .= '<td></td>';
                   $html_report .= '</tr>';
