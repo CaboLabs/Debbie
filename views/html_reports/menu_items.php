c921d68bf0876237bb0cbbb770d79c5e458dc56e
ruthbri <ruthbri@gmail.com>
2024-03-13 20:52:42 -0300

    conflict resolved #20

--- views/html_reports/menu_items.php
+++ views/html_reports/menu_items.php
@@ -1,12 +1,12 @@
 <li class="nav-item">
   <a id="<?=$item?>" class="nav-link collapsed" href="#" data-toggle="collapse"
-  data-target="#collapseUtilities_<?=$item?>" aria-expanded="true" aria-controls="collapseUtilities">
-  <?php if ($is_failed): ?>
-  <i class="fas fa-times text-warning"></i>
-  <?php else: ?>
-  <i class="fa fa-check text-success"></i>
-  <?php endif; ?>
-    <span>
+    data-target="#collapseUtilities_<?=$item?>" aria-expanded="true" aria-controls="collapseUtilities">
+    <?php $icon = '<i class="fa fa-check text-success"></i>' ?>
+    <?php if ($is_failed): ?>
+      <?php $icon = '<i class="fas fa-times text-warning"></i>' ?>
+    <?php endif ?>
+    <?=$icon?>
+    <span class="d-inline-block text-truncate" style="max-width: 150px;">
       <?=$item?>
     </span>
   </a>
@@ -15,7 +15,7 @@
     <?php foreach ($namesSuitessubmenu as $submenu): ?>
       <?php $suites = explode("\\", $submenu); ?>
       <?php if (in_array($item, $suites)): ?>
-        <a class="collapse-item" href="#"><?=$suites[2]?></a>
+        <a class="d-inline-block text-truncate collapse-item" style="max-width: 165px;" href="#"><?=$suites[2]?></a>
       <?php endif; ?>
     <?php endforeach; ?>
     </div>
