b8149a8fd66e9912baf59af9dd451059491ce4bf
ruthbri <ruthbri@gmail.com>
2024-03-14 19:36:36 -0300

    conflict resolved #14

--- views/html_reports/menu_items.php
+++ views/html_reports/menu_items.php
@@ -1,21 +1,21 @@
 <li class="nav-item">
   <a id="<?=$item?>" class="nav-link collapsed" href="#" data-toggle="collapse"
-  data-target="#collapseUtilities_<?=$item?>" aria-expanded="true" aria-controls="collapseUtilities">
-  <?php if ($is_failed): ?>
-  <i class="fas fa-times text-warning"></i>
-  <?php else: ?>
-  <i class="fa fa-check text-success"></i>
-  <?php endif; ?>
-    <span>
-      <?=$item?>
-    </span>
+    data-target="#collapseUtilities_<?=$item?>" aria-expanded="true" aria-controls="collapseUtilities">
+    <?php $style_item = "fa fa-check text-success" ?>
+    <?php $style_text = '"color:green"'; ?>
+    <?php if ($is_failed): ?>
+      <?php $style_item = "fas fa-times text-warning" ?>
+      <?php $style_text = '"color:red"'; ?>
+    <?php endif; ?>
+    <i class="<?=$style_item?>"></i>
+    <span class="mr-1"><?=$item?></span>
   </a>
   <div id="collapseUtilities_<?=$item?>" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
     <div id="collapse_<?=$item?>" class="bg-white py-2 collapse-inner rounded">
     <?php foreach ($namesSuitessubmenu as $submenu): ?>
       <?php $suites = explode("\\", $submenu); ?>
       <?php if (in_array($item, $suites)): ?>
-        <a class="collapse-item" href="#"><?=$suites[2]?></a>
+        <a id="<?=$suites[2]?>" class="collapse-item" style=<?=$style_text?> href="#"><?=$suites[2]?></a>
       <?php endif; ?>
     <?php endforeach; ?>
     </div>
