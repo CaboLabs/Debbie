746654b955066f5d15269baeb76128129156e63a
ruthbri <ruthbri@gmail.com>
2024-05-21 17:06:39 -0300

    add new tests with warning #38

---
+++ tests/suite5/TestCase51.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace tests\suite5;
+
+use \CaboLabs\Debbie\DebbieTestCase;
+
+class TestCase51 extends DebbieTestCase {
+
+   public function test_does_not_file()
+   {
+      $directory = '/false/path/file.txt';
+
+      if ($handle = opendir($directory)) {
+
+          while (false !== ($entrada = readdir($handle))) {
+              echo "$entrada\n";
+          }
+          closedir($handle);
+      }
+   }
+
+   public function test_warning()
+   {
+    trigger_error("This is an intentional warning", E_USER_WARNING);
+   }
+}
+
+?>
