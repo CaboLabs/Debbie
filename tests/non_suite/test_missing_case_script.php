bc2e5f0251b12b68c65cf100a36bb07ddb9e935d
ruthbri <ruthbri@gmail.com>
2025-07-30 13:49:50 -0300

    add commets #38

--- tests/non_suite/test_missing_case_script.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-use CaboLabs\Debbie\DebbieRun;
-
-$runner = new DebbieRun();
-$runner->init('./tests');
-// Simulates executing a test case that does not exist
-$runner->run_case('suite_missing_file', 'TestCaseMissing');
